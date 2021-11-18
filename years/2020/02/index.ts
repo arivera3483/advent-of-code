5198e05e849d508be7309ffc04b522dde8d6df7c
Adam Rivera <adam@riverafamily.xyz>
2021-11-18 10:11:24 -0500

    success day 2

--- years/2020/02/index.ts
+++ years/2020/02/index.ts
@@ -13,13 +13,53 @@ const DAY = 2;
 // problem url  : https://adventofcode.com/2020/day/2
 
 async function p2020day2_part1(input: string, ...params: any[]) {
-	const lines = input.split("\n");
+	const lines = input.split("\n").map(String);
 
-	return "Not implemented";
+	var count = 0;
+
+	for (let i = 0; i < lines.length; i++) {
+		// for each line, split it by space. 0 == min-max number of chars; 1 == char, 2 == password
+		var item = lines[i].split(" ");
+		//set min and max. each can be a 2 digit number
+		var range = item[0].split("-").map(Number);
+		//get character needed
+		var chara = item[1].substr(0, 1);
+		var re = new RegExp(chara, "g")
+		var numReq = (item[2].match(re) || []).length;
+		if (numReq >= range[0] && numReq <= range[1]) {
+			count++;
+		}
+	}
+
+	return count;
 }
 
 async function p2020day2_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n").map(String);
+
+	var count = 0;
+
+	for (let i = 0; i < lines.length; i++) {
+		// for each line, split it by space. 0 == min-max number of chars; 1 == char, 2 == password
+		var item = lines[i].split(" ");
+		//set min and max. each can be a 2 digit number
+		var range = item[0].split("-").map(Number);
+		//get character needed
+		var chara = item[1].substr(0, 1);
+		var re = new RegExp(chara, "g");
+		var p1 = item[2].substring(range[0] - 1, range[0]);
+		var p2 = item[2].substring(range[1] - 1, range[1]);
+
+		if ((p1 == chara || p2 == chara) && !(p1 == chara && p2 == chara)) {
+			count++;
+		}
+		//var numReq = (item[2].match(re) || []).length;
+		//if (numReq >= range[0] && numReq <= range[1]) {
+		//	count++;
+		//}
+	}
+
+	return count;
 }
 
 async function run() {
