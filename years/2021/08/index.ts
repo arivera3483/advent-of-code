eadf0bb08241921eba7bbaf6bcef3049d93e809b
Adam Rivera <adam@riverafamily.xyz>
2021-12-08 23:20:27 -0500

    d8 p2 - github/yayukitano solution w filters

--- years/2021/08/index.ts
+++ years/2021/08/index.ts
@@ -50,18 +50,51 @@ async function p2021day8_part1(input: string, ...params: any[]) {
 	return count;
 }
 
+function getDigitPatterns(input: string[]) {
+	input.sort((a, b) => a.length - b.length);
+	let key:string[] = ["","","","","","","","","",""]
+	key[1] = input.shift() as string;
+	key[7] = input.shift() as string;
+	key[4] = input.shift() as string;
+	key[8] = input.pop() as string;
+	let threeTwoFive = input.slice(0,3);
+	let zeroNineSix = input.slice(3,6);
+	key[6] = zeroNineSix.filter(e => !([...key[1]].every(f => e.includes(f))))[0];
+	zeroNineSix = zeroNineSix.filter(e => e !== key[6]);
+	key[9] = zeroNineSix.filter(e => [...key[4]].every(f => e.includes(f)))[0];
+	key[0] = zeroNineSix.filter(e => e !== key[9])[0];
+	key[3] = threeTwoFive.filter(e => [...key[1]].every(f => e.includes(f)))[0];
+	threeTwoFive = threeTwoFive.filter(e => e !== key[3]);
+	key[5] = threeTwoFive.filter(e => [...e].every(f => key[6].includes(f)))[0];
+	key[2] = threeTwoFive.filter(e => e !== key[5])[0];
+	return key.map(e => [...e].sort().join(""));
+}
+
 async function p2021day8_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n").map(e => e.split(" | ").map(e => e.split(/\s+/)));
+	let num = 0;
+
+	for (let display of lines) {
+		const key = Object.fromEntries([...getDigitPatterns(display[0]).entries()].map(e => [e[1], e[0]]));
+		let digits = "";
+		for (let output of display[1].map(e => [...e].sort().join(""))) {
+			digits += key[output].toString();
+		}
+
+		num += parseInt(digits, 10);
+
+	}	
+	return num;
 }
 
 async function run() {
 	const part1tests: TestCase[] = [{
-		input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagcecdfeb fcadb cdfeb cdbaf`,
+		input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`,
 		extraArgs: [],
 		expected: `26`
 	}];
 	const part2tests: TestCase[] = [{
-		input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagcecdfeb fcadb cdfeb cdbaf`,
+		input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\nfgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\nfbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\naecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\nfgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\ndbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\nbdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\negadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\ngcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`,
 		extraArgs: [],
 		expected: `61229`
 	}];
